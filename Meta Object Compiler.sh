50ed99a6a8e450546f08eb1f504a0fed39c658d4
dak180 <dak180@users.sourceforge.net>
2010-05-14 16:00:40 -0400

    Better error messages when moc is missing.

--- Meta Object Compiler.sh
+++ Meta Object Compiler.sh
@@ -1,7 +1,11 @@
+export PATH=$PATH:/sw/bin:/opt/local/bin
 echo "Checking for moc..."
 if ! type -aP moc; then
-    echo "error: Fatal inability to properly moc the code." >&2
+    echo "error: Fatal inability to properly moc the code. (Insure you have installed Qt or risk more mocing.)" >&2
+    open ./README.BUILD.txt
+    exit 1
+elif ! moc -v; then
+    echo "error: Moc is not properly installed" >&2
     exit 1
 fi
-
 exit 0
