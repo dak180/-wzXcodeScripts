ca9ba656abd651bef5e2f700438e01d566caac59
dak180 <dak180@users.sourceforge.net>
2010-05-15 17:01:00 -0400

    A better test for moc since moc cannot be bothered to give a zero exit status when asked its version.

--- Meta Object Compiler.sh
+++ Meta Object Compiler.sh
@@ -1,11 +1,12 @@
-export PATH=$PATH:/sw/bin:/opt/local/bin
+export PATH=/sw/lib/qt4-mac/bin:/opt/local/bin:${PATH}
 echo "Checking for moc..."
 if ! type -aP moc; then
     echo "error: Fatal inability to properly moc the code. (Insure you have installed Qt or risk more mocing.)" >&2
     open ./README.BUILD.txt
     exit 1
-elif ! moc -v; then
+elif [[ `moc -v 2>&1 | sed -e 's:.*Qt \(4\.6\)\..)$:\1:'` < "4.6" ]]; then
     echo "error: Moc is not properly installed" >&2
     exit 1
 fi
+moc -v
 exit 0
