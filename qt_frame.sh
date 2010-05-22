cb856d50facbe51ad71c158e6bd098b7becde69b
dak180 <dak180@users.sourceforge.net>
2010-05-21 23:04:46 -0400

    Add qt_frame.

---
+++ qt_frame.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Config
+go_dir=`dirname ${BASH_SOURCE}`
+flist=`ls -1 "${go_dir}" | sed -n 's:.framework$:&:p'`
+mjrver="4.0"
+olmjrver="4"
+
+
+cd "${go_dir}"
+
+for Qtframe in ${flist}; do
+    cd "${Qtframe}"
+    Qtname=`basename "${Qtframe}" ".framework"`
+    echo "Fixing up ${Qtname}..."
+    
+    # Put the Info.plist in the right place
+    mkdir -pm "0775" "Versions/${mjrver}/Resources"
+    mv -n "Contents/Info.plist" "Versions/${mjrver}/Resources/"
+    rmdir "Contents"
+    
+    # Correct the symlinks
+    ln -sfh "Versions/Current/Headers" "Headers"
+    ln -sfh "Versions/Current/Resources" "Resources"
+    ln -sfh "Versions/Current/${Qtname}" "${Qtname}"
+    
+    
+    # Set the names correctly
+    install_name_tool -id @executable_path/../Frameworks/${Qtframe}/Versions/${mjrver}/${Qtname} "${Qtname}"
+    if [[ ! "${Qtname}" = "QtCore" ]]; then
+        install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore "${Qtname}"
+    fi
+    if [[ "${Qtname}" = "QtOpenGL" ]]; then
+        install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4.0/QtGui "${Qtname}"
+    fi
+    
+    cd ..
+done
+
+echo "Remember to check that the info.plist files are not borked."
+
+exit 0
