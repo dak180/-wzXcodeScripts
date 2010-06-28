b8caf34fbf10906adf0a1621335ab52487e4aa5b
dak180 <dak180@users.sourceforge.net>
2010-06-28 16:27:31 -0400

    Update libping to ver. 1.4.3.

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libpng-1.4.2"
+DirectorY="libpng-1.4.3"
 OutDir="libpng"
-FileName="libpng-1.4.2.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/01-libpng-master/1.4.2/libpng-1.4.2.tar.gz"
-MD5Sum="89fd334dc5fc84ff146b9269c4fa452f"
+FileName="libpng-1.4.3.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/01-libpng-master/1.4.3/libpng-1.4.3.tar.gz"
+MD5Sum="df3521f61a1b8b69489d297c0ca8c1f8"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
