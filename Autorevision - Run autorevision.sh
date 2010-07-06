f9f43a1d2ea73f98ab91e111d1d4b63cc12751ef
dak180 <dak180@users.sourceforge.net>
2010-07-06 14:19:16 -0400

    Make the autorevision script smarter.

--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -1,3 +1,19 @@
 cd ..
-./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h
+
+# Wait a bit if Autorevision does not exist
+tick=0
+while [ ! -s "./macosx/build/${CONFIGURATION}/Autorevision" ]; do
+    let tick=${tick}+1
+    if [ ${tick} == 11 ]; then
+        break
+    fi
+    sleep 1
+done
+
+
+if ! ./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h; then
+    echo "error: Could not run Autorevision"
+    exit 1
+fi
+
 sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:tags/::' -e 's:branches/\(.*\)$:\1 (SVN_REV):' -e 's:trunk:Trunk (SVN_REV):' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
