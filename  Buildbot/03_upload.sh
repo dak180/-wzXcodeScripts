9fe371b7c40e95017602b655523f029a37807e44
dak180 <dak180@users.sourceforge.net>
2010-09-18 21:58:34 -0400

    Link a current dSYM bundle too.

---  Buildbot/03_upload.sh
+++  Buildbot/03_upload.sh
@@ -27,5 +27,5 @@ if ! scp -lpq 160 ${lpth}${dmg_bn}${dmg_nv} ${uurl}:${rpth}${dmg_bn}${bran}${rev
 fi
 
 
-# Link up the current .dmg
-ssh ${uurl} -C "cd ${rpth} && ln -fs ${dmg_bn}${bran}${revt}.dmg ${dmg_bn}${bran}-current.dmg"
+# Link up the current .dmg and dSYM bundle
+ssh ${uurl} -C "cd ${rpth} && ln -fs ${dmg_bn}${bran}${revt}.dmg ${dmg_bn}${bran}-current.dmg && ln -fs ${dmg_bn}${bran}${revt}${tar_dS} ${dmg_bn}${bran}-current${tar_dS}"
