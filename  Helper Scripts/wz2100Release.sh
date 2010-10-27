5d0af04afb0b9484921cd11fce91a083aff54422
dak180 <dak180@users.sourceforge.net>
2010-10-27 14:56:52 -0400

    Get the version system working with git.

---  Helper Scripts/wz2100Release.sh
+++  Helper Scripts/wz2100Release.sh
@@ -3,8 +3,7 @@
 # Config
 rtag="${1}"
 prerun="${2}"
-pathd="${HOME}/Applications/Build/wz2100/tags"
-urld="https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/tags/"
+pathd="${HOME}/Applications/Build/wz2100/hg/wz2100-git"
 dmg_bn="warzone2100"
 dmg_nv="-novideo.dmg"
 dmg_lv="-lqvideo.dmg"
@@ -19,15 +18,14 @@ fi
 
 # Checkout
 cd "${pathd}"
-if ! svn checkout ${urld}${rtag} ${rtag}; then
-    echo "Unable to fetch ${rtag}"
-    cd ${rtag}
-    svn cleanup
+if ! git checkout -f ${rtag}; then
+    echo "Unable to checkout ${rtag}"
     exit 1
 fi
+git_sr=`git rev-parse -q --short --verify HEAD`
 
 # Build
-cd ${rtag}/macosx
+cd ./macosx
 if [ "${prerun}" == "pre" ]; then
     xcodebuild -project Warzone.xcodeproj -target Warzone -configuration Release -PBXBuildsContinueAfterErrors=NO
     open .
@@ -43,29 +41,29 @@ fi
 cd build/dmgout/out
 
 if [ -f "${dmg_bn}${dmg_nv}" ]; then
-    mv "${dmg_bn}${dmg_nv}" "${dmg_bn}-${rtag}${dmg_nv}"
-    md5 -q "${dmg_bn}-${rtag}${dmg_nv}">"${dmg_bn}-${rtag}${dmg_nv}.md5"
+    mv "${dmg_bn}${dmg_nv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}"
+    md5 -q "${dmg_bn}-${rtag}${dmg_nv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}.md5"
 else
     echo "${dmg_bn}${dmg_nv} does not exist, skipping"
 fi
 
 if [ -f "${dmg_bn}${dmg_lv}" ]; then
-    mv "${dmg_bn}${dmg_lv}" "${dmg_bn}-${rtag}${dmg_lv}"
-    md5 -q "${dmg_bn}-${rtag}${dmg_lv}">"${dmg_bn}-${rtag}${dmg_lv}.md5"
+    mv "${dmg_bn}${dmg_lv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}"
+    md5 -q "${dmg_bn}-${rtag}${dmg_lv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}.md5"
 else
     echo "${dmg_bn}${dmg_lv} does not exist, skipping"
 fi
 
 if [ -f "${dmg_bn}${dmg_hv}" ]; then
-    mv "${dmg_bn}${dmg_hv}" "${dmg_bn}-${rtag}${dmg_hv}"
-    md5 -q "${dmg_bn}-${rtag}${dmg_hv}">"${dmg_bn}-${rtag}${dmg_hv}.md5"
+    mv "${dmg_bn}${dmg_hv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}"
+    md5 -q "${dmg_bn}-${rtag}${dmg_hv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}.md5"
 else
     echo "${dmg_bn}${dmg_hv} does not exist, skipping"
 fi
 
 if [ -f "${dmg_bn}${tar_dS}" ]; then
-    mv "${dmg_bn}${tar_dS}" "${dmg_bn}-${rtag}${tar_dS}"
-    md5 -q "${dmg_bn}-${rtag}${tar_dS}">"${dmg_bn}-${rtag}${tar_dS}.md5"
+    mv "${dmg_bn}${tar_dS}" "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}"
+    md5 -q "${dmg_bn}-${rtag}${tar_dS}">"${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}.md5"
 else
     echo "${dmg_bn}${tar_dS} does not exist, skipping"
 fi
--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -17,4 +17,4 @@ if ! ./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h; th
     exit 1
 fi
 
-sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:tags/::' -e 's:branches/\(.*\)$:\1 (SVN_REV):' -e 's:trunk:Trunk (SVN_REV):' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
+sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
