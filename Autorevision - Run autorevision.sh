85d52ede22cb3a6a9f60add8857b98569dbaab8e
dak180 <dak180@users.sourceforge.net>
2010-10-31 23:22:11 -0400

    Drop the "v" on tags.

--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -17,4 +17,4 @@ if ! ./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h; th
     exit 1
 fi
 
-sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
+sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
