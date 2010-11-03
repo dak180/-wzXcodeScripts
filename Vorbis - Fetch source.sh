6ff028fe32659a18e52815fcd5901d610017c73b
dak180 <dak180@users.sourceforge.net>
2010-11-03 12:37:42 -0400

    Update vorbis to 1.3.2.

--- Vorbis - Fetch source.sh
+++ Vorbis - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libvorbis-1.3.1"
+DirectorY="libvorbis-1.3.2"
 OutDir="libvorbis"
-FileName="libvorbis-1.3.1.tar.gz"
-SourceDLP="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz"
-MD5Sum="016e523fac70bdd786258a9d15fd36e9"
+FileName="libvorbis-1.3.2.tar.gz"
+SourceDLP="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.2.tar.gz"
+MD5Sum="c870b9bd5858a0ecb5275c14486d9554"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
