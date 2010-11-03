60f01cff6971e076c223b0a41f9c709b78f41ba7
dak180 <dak180@users.sourceforge.net>
2010-11-03 12:36:57 -0400

    Update ogg to 1.2.1.

--- Ogg - Fetch source.sh
+++ Ogg - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libogg-1.2.0"
+DirectorY="libogg-1.2.1"
 OutDir="libogg"
-FileName="libogg-1.2.0.tar.gz"
-SourceDLP="http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz"
-MD5Sum="c95b73759acfc30712beef6ce4e88efa"
+FileName="libogg-1.2.1.tar.gz"
+SourceDLP="http://downloads.xiph.org/releases/ogg/libogg-1.2.1.tar.gz"
+MD5Sum="b998c2420721146df3b3d0e7776c97b9"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
