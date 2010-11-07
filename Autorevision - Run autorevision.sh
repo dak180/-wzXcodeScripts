e989f9647bc6d4576f03853e1b5a53c8e63d2454
dak180 <dak180@users.sourceforge.net>
2010-11-07 15:57:54 -0500

    Try to work from a tarball.

--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -1,5 +1,19 @@
+function hfilter {
+    sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
+}
+
+
 cd ..
 export PATH=$PATH:/sw/bin:/opt/local/bin
+
+# Do not run if we will not get useful information
+if [[ ! -d ".git" ]] && [[ ! -d ".hg" ]]; then
+    if [ -f "src/autorevision.h" ]; then
+        echo "Not a repo."
+        hfilter
+        exit 0
+    fi
+fi
 
 # Wait a bit if Autorevision does not exist
 tick=0
@@ -17,4 +31,5 @@ if ! ./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h; th
     exit 1
 fi
 
-sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
+hfilter
+exit 0
