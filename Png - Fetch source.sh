fdd0d371c13a9a770ea32ee2011244a61aadb5fc
dak180 <dak180@users.sourceforge.net>
2010-12-11 14:46:02 -0500

    png changed their url again.

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,7 +1,7 @@
 DirectorY="libpng-1.4.4"
 OutDir="libpng"
 FileName="libpng-1.4.4.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng-master/1.4.4/libpng-1.4.4.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng14/1.4.4/libpng-1.4.4.tar.gz"
 MD5Sum="297b38f925e745061489b41b1f7c4bb1"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
