f21330ab9846caa4de86221a979f48d12df89825
dak180 <dak180@users.sourceforge.net>
2010-12-19 19:14:14 -0500

    Upgrade png to 1.4.5; I really wish they would stop changing the download paths.

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libpng-1.4.4"
+DirectorY="libpng-1.4.5"
 OutDir="libpng"
-FileName="libpng-1.4.4.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng14/1.4.4/libpng-1.4.4.tar.gz"
-MD5Sum="297b38f925e745061489b41b1f7c4bb1"
+FileName="libpng-1.4.5.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng14/1.4.5/libpng-1.4.5.tar.gz"
+MD5Sum="dd4175393720ae041c67ace87cf6d212"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
