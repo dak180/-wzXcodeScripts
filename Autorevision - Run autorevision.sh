1ecc3c161fe6498af814bda96808a22e8b51e20e
dak180 <dak180@users.sourceforge.net>
2011-03-06 20:26:49 -0500

    Use the autorevision script instead of the program.

--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -1,10 +1,10 @@
 function hfilter {
-    sed -n 's:#define:&:p' src/autorevision.h | sed 's:"::g' | sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' >macosx/build/autorevision.h
+    sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' src/autorevision.h | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' > "${OBJROOT}/autorevision.h"
 }
 
 
-cd ..
 export PATH=$PATH:/sw/bin:/opt/local/bin
+cd ..
 
 # Do not run if we will not get useful information
 if [[ ! -d ".git" ]] && [[ ! -d ".hg" ]]; then
@@ -15,18 +15,8 @@ if [[ ! -d ".git" ]] && [[ ! -d ".hg" ]]; then
     fi
 fi
 
-# Wait a bit if Autorevision does not exist
-tick=0
-while [ ! -s "./macosx/build/${CONFIGURATION}/Autorevision" ]; do
-    let tick=${tick}+1
-    if [ ${tick} == 11 ]; then
-        break
-    fi
-    sleep 1
-done
 
-
-if ! ./macosx/build/${CONFIGURATION}/Autorevision +cstr . src/autorevision.h; then
+if ! build_tools/autorevision.sh src/autorevision.h; then
     echo "error: Could not run Autorevision"
     exit 1
 fi
