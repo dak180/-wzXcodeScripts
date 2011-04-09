5cfad2700b0f76e49ed0e0f639b3e8bb8d2316d1
dak180 <dak180@users.sourceforge.net>
2011-04-09 16:56:07 -0400

    Update png to 1.4.6.

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libpng-1.4.5"
+DirectorY="libpng-1.4.6"
 OutDir="libpng"
-FileName="libpng-1.4.5.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng14/1.4.5/libpng-1.4.5.tar.gz"
-MD5Sum="dd4175393720ae041c67ace87cf6d212"
+FileName="libpng-1.4.6.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng14/1.4.6/libpng-1.4.6.tar.gz"
+MD5Sum="061c30db9be127b0abfdd468fa27528f"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
