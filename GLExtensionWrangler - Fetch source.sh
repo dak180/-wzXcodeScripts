93513500f2d76a5bd0d11f3c6c1677828bf8a94e
dak180 <dak180@users.sourceforge.net>
2011-05-17 13:13:07 -0400

    Update glew to 1.6.0.

--- GLExtensionWrangler - Fetch source.sh
+++ GLExtensionWrangler - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="glew-1.5.7"
+DirectorY="glew-1.6.0"
 OutDir="glew"
-FileName="glew-1.5.7.tgz"
-SourceDLP="http://downloads.sourceforge.net/project/glew/glew/1.5.7/glew-1.5.7.tgz"
-MD5Sum="f913ce9dbde4cd250b932731b3534ded"
+FileName="glew-1.6.0.tgz"
+SourceDLP="http://downloads.sourceforge.net/project/glew/glew/1.6.0/glew-1.6.0.tgz"
+MD5Sum="7dfbb444b5a4e125bc5dba0aef403082"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
