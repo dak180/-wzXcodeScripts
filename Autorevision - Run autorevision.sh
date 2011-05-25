bc44baaf51aee94c01c65b83b90b7dc67e3c6d74
dak180 <dak180@users.sourceforge.net>
2011-05-25 17:31:12 -0400

    Refine the autorevision.h update process and avoid updating if we do not have to.

--- Autorevision - Run autorevision.sh
+++ Autorevision - Run autorevision.sh
@@ -1,24 +1,38 @@
+# Config
+export PATH=$PATH:/sw/bin:/opt/local/bin
 function hfilter {
-    sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' src/autorevision.h | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' > "${OBJROOT}/autorevision.h"
+	sed -e 's:refs/heads/:branch/:' -e 's:master:Master:' -e 's:^v::' src/autorevision.h | sed -e 's:_beta: Beta :' -e 's:_rc: RC :' > "${OBJROOT}/autorevision.h"
+}
+sauto="src/autorevision.h"
+tauto="${OBJROOT}/autorev/autorevision.h"
+function bauto {
+	if ! build_tools/autorevision.sh "${tauto}"; then
+		echo "error: Could not run Autorevision"
+		exit 1
+	fi
 }
 
-
-export PATH=$PATH:/sw/bin:/opt/local/bin
 cd ..
 
-# Do not run if we will not get useful information
-if [[ ! -d ".git" ]] && [[ ! -d ".hg" ]]; then
-    if [ -f "src/autorevision.h" ]; then
-        echo "Not a repo."
-        hfilter
-        exit 0
-    fi
-fi
-
-
-if ! build_tools/autorevision.sh src/autorevision.h; then
-    echo "error: Could not run Autorevision"
-    exit 1
+if [[ ! -d ".git" ]] && [[ ! -d ".hg" ]] && [[ -f "${sauto}" ]]; then
+	# Do not run if we will not get useful information
+	echo "Not a repo."
+	hfilter
+	exit 0
+elif [[ -f "${sauto}" ]] && [[ -f "${tauto}" ]]; then
+	bauto
+	mdck1=`md5 -q "${sauto}"`
+	mdck2=`md5 -q "${tauto}"`
+	if [ "${mdck1}" = "${mdck2}" ]; then
+		exit 0
+	else
+		cp -a "${tauto}" "${sauto}"
+	fi
+else
+	# Only update autorevision.h if something has changed
+	mkdir "${OBJROOT}/autorev/"
+	bauto
+	cp -a "${tauto}" "${sauto}"
 fi
 
 hfilter
