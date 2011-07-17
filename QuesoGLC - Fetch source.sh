b8240a3d87825b7afc5fcf536bfbfa5ae491a336
dak180 <dak180@users.sourceforge.net>
2011-07-17 10:14:47 -0400

    Minor edits to QuesoGLC.

--- QuesoGLC - Fetch source.sh
+++ QuesoGLC - Fetch source.sh
@@ -7,4 +7,4 @@ MD5Sum="ee73eebd23292dfb17365ff19eb15a0e"
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
 
-# tar -czf quesoglc-0.7-r921.tar.gz --exclude '.svn' --exclude 'Makefile*' --exclude 'makefile*' --exclude '.DS_Store'  quesoglc-0.7-r921
+# tar -czf quesoglc-0.7-r921.tgz --exclude '.svn' --exclude '.DS_Store' quesoglc-0.7-r921
