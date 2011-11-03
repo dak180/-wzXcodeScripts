507bdc832f5291891833acffa8b33930dca0ee09
dak180 <dak180@users.sourceforge.net>
2011-11-03 10:08:25 -0400

    Update Png to 1.5.6.
    --HG--
    extra : signature : gnupg%3AiQIcBAABAgAGBQJOsqBaAAoJEIFZtS7M14OuoJ8P/iLgTpxylCGn5ksVZsuLNwWL1z6UJ8jqrJcOoEuYrxhIjCHybope/cf7Jd7cElXErokXPDiFFNh2v9pEZLuUY3RlhxXjFiTWgjdXiMf99N3nNMGjiA7tMBnVPIJ1w8UC0vi51T8fjnLqhvp577D5uTZof5PEFr0E29jSjIRgnCgTSyJ3%2BAT54m%2BKeItyEQW52oTrrjVjNsmqW41LbCcRMGuDx8erwc28pWu5cXmU4Zes/q0g/k8KoqHZjrfRtlMo0Q4UxHNek7pmzsLFw5/fQ7Q9tAZ3QoTqaTRCx5wCT0xQbTqIHp7JFVIX0UImIcySxEMKqMizGnio0o9cuK7FDQVm1IS8OawKBA2KnoZHHr3fSU8YcGjWni1HL70t/r4VE7Kdlu5Gvz0K/7uGFEG1uHiJlcIFQ2jmRAL%2BdhZkJ%2BqflIFRNEg6M3%2B1rxegG9GiUODAiqKxi3%2Bz1n%2B51jaMCLw1KidMaBtPZoXPmK4jneU1NFIbtGoX3V8MnlCKDT9Fwd/xK/gyzNWPn0mFUxiXVnLs5xg5v3TUGzPKOBKPcl0S3kMJnITUuNX01tPuX96JjY5owuK5z9Qk/NtI4wcIRZ9lCkpVAznx2PN5A/FOdPn1gcak6fpVEhbG4f3KuJ8IDue2%2B%2B5q9rcImz574VN%2BFnTt1KZr49dv3iwiOayVvWQs

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libpng-1.5.5"
+DirectorY="libpng-1.5.6"
 OutDir="libpng"
-FileName="libpng-1.5.5.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/1.5.5/libpng-1.5.5.tar.gz"
-MD5Sum="003bcac022125029bae4818d74c42a94"
+FileName="libpng-1.5.6.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/1.5.6/libpng-1.5.6.tar.gz"
+MD5Sum="8b0c05ed12637ee1f060ddfbbf526ea3"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
