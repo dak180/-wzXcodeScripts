2f545bd38ef54ebd1c84d38ac9f2baa145816088
dak180 <dak180@users.sourceforge.net>
2012-01-27 17:48:59 -0500

    Update the docs for the xcode builds.
    --HG--
    extra : signature : gnupg%3AiQIcBAABCgAGBQJPIynkAAoJEIFZtS7M14OuHqcQALm1vdAYreoI9cOvrGNPXZADmJNLpPduGm5IwjencdhBGs2d03vpozTC4D6g4ZeBo8clo61TQFTaMmNHbCHX4u6S06HBdUjNjx2D8na0cnocT56tUtbyk032VLMMNbySuBN8NWOp6MKfK%2Bsg0n309WDwLRfWpSbg5LUBAVLtdXaNNLeEs3jwl0zcs1RJ0/CVPfs9mCWUzWrty4h1eRNSODABcQE0RYu5lu0JsBYXU43qzcoCZ1NlPFgXLy%2B/kQ46VxRcTzB7FH6k16Dnz6/GMXoOhFO4oLvHI1T/7YOc69K%2BK3CqRwLtw7JM2GrTbNnemOO0b1mPtcsW/XN3jDuTJUf5GKLSHD/lB0oMv1C01kktqxqB8WlVuQ7%2B3QMQ9DfYq28eqj1hJDmOXQ2itim2lOS6MPL92ZK4a2P%2BGhX90g6kHbCvHlp5LHlgxi7iFfcXTpXR9xYa1uBbXr0MxenD54lGypuOZI9waTo0vO2a7OZeVgJCAlQLdTl30hYxOKe1THMkYZOaAo10VPrfrdID71n6DiUJHlqdrmwzzJxjg48nCN%2BwZF5eQGhcpsx7VweofPUEwNFn3fUSo8f7qZdgawmb/0oshcWwfLBWY4GiYg%2BG6PRbvm%2Bsclqta7VEexMpBfFHISGDQrLTQTgk4%2BYKwfVtMd0LIvVYLR/ls0dHCVbk

--- Setup Prebuilt Components - Docs.sh
+++ Setup Prebuilt Components - Docs.sh
@@ -1,8 +1,8 @@
-DirectorY="WarzoneHelp-287be0d"
+DirectorY="WarzoneHelp-3fda3de"
 OutDir="WarzoneHelp"
-FileName="WarzoneHelp-287be0d.tgz"
-BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/WarzoneHelp-287be0d.tgz"
-MD5Sum="909bc7f5de29a7bb579c6a77b5a4bb9b"
+FileName="WarzoneHelp-3fda3de.tgz"
+BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/WarzoneHelp-3fda3de.tgz"
+MD5Sum="d5affe84156990e9e7c45066bf8ef5c2"
 
 configs/FetchPrebuilt.sh "${DirectorY}" "${OutDir}" "${FileName}" "${BuiltDLP}" "${MD5Sum}"
 exit ${?}
