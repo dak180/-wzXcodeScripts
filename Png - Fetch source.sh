4435ba0a72abffa42e868bf102786e30254d3695
dak180 <dak180@users.sourceforge.net>
2012-02-01 10:06:56 -0500

    Update Zlib to 1.2.6 and Png to 1.5.8.
    --HG--
    extra : signature : gnupg%3AiQIcBAABCgAGBQJPKVUWAAoJEIFZtS7M14Ouxh0P/22t8ocQHYqcwXclOxX56LaErPKOZPq/42ZBH%2BlxzUu2pWrhzpi05z35QXyIOrWpU0lBWnlZmjvG5xxg1blw7x770Lwj7G1pPOV1JBdAOhX716QKwHDxGIC6g6RMdkJh6F7LUUeuCPkC1dprzgomSPUZ/XYo6Q27vTVU3P2G2EWK/eb8DBOewYN4RwqEbrgqZDOFavJESxvTO2bWfwJUWEsO3kkZ6hg8kPf0N64Izy2GKAJEYYal7uWcVExdRTU8LYk6kc/v/L1PAHJN2/jjRwyzFkuyq2%2BxxEcI/u5bzo0BU/EbY3Ur5qpxA4WC%2B8I9NUcBHfNsaa%2BjR62/575r4Z9ws44jo3jZyMpFa92RFkrmI7piySOmyefcqEXs/FlzSZflxsFzlJa51UHDG%2B0oe1iG2sjby4qptAnWJjqvrzH5rOnVmQSdG1IFaiyxsmMfo1lQn%2BY7FWjxCFVCc4mdySEPn4oRbdr58YBja6MD2DGGi8%2BF6w%2B5f/CHwxj2K9MXanHgU2rFJoMieDnutzIL8vwPAJ0CFGKEmiOJehpHnM8kaFFaFE21FSI95ZKiJx9QG5IoGN2r%2BXCAhW%2Boz45MUhWMKYmxBmTERgpWh2dpR7xcKrMqua3mM%2BQ2CwArBJoJ411txX/LW8qhYh1Si/3sxHgO0sy/1Czxg8iMsMJvc5Ho

--- Png - Fetch source.sh
+++ Png - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="libpng-1.5.7"
+DirectorY="libpng-1.5.8"
 OutDir="libpng"
-FileName="libpng-1.5.7.tar.gz"
-SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/1.5.7/libpng-1.5.7.tar.gz"
-MD5Sum="944b56a84b65d94054cc73d7ff965de8"
+FileName="libpng-1.5.8.tar.gz"
+SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/1.5.8/libpng-1.5.8.tar.gz"
+MD5Sum="dc2b84a1c077531ceb5bf9d79ad889a4"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
--- Zlib - Fetch source.sh
+++ Zlib - Fetch source.sh
@@ -1,8 +1,8 @@
-DirectorY="zlib-1.2.5"
+DirectorY="zlib-1.2.6"
 OutDir="zlib"
-FileName="zlib-1.2.5.tar.gz"
-SourceDLP="http://zlib.net/zlib-1.2.5.tar.gz"
-MD5Sum="c735eab2d659a96e5a594c9e8541ad63"
+FileName="zlib-1.2.6.tar.gz"
+SourceDLP="http://zlib.net/zlib-1.2.6.tar.gz"
+MD5Sum="618e944d7c7cd6521551e30b32322f4a"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
