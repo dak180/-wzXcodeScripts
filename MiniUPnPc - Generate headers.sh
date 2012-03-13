2059d51315cf4e5b63b527ea1d1ad8846dd43dd0
dak180 <dak180@users.sourceforge.net>
2012-03-12 22:17:59 -0400

    Remove obsolescent scripts.

--- MiniUPnPc - Generate headers.sh
+++
@@ -1,4 +0,0 @@
-cd ../3rdparty/miniupnpc
-
-sed -e "s:OS/version:Mac OS:" miniupnpcstrings.h.in >"${DERIVED_FILE_DIR}/miniupnpcstrings.h"
-exit ${?}
--- QuesoGLC - Create database.c.sh
+++
@@ -1,8 +0,0 @@
-cd external/quesoglc
-
-if [ ! -r src/database.c ]; then
-	cd database
-	/usr/bin/python buildDB.py
-	exit ${?}
-fi
-exit 0
