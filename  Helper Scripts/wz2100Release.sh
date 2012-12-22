699c8624644075bc093a145f879d6a2210472d94
dak180 <dak180@users.sourceforge.net>
2012-12-22 00:22:33 -0500

    Update for new output.
    Also improve formatting.

---  Helper Scripts/wz2100Release.sh
+++  Helper Scripts/wz2100Release.sh
@@ -8,7 +8,7 @@ dmg_bn="warzone2100"
 dmg_nv="-novideo.dmg"
 dmg_lv="-lqvideo.dmg"
 dmg_hv="-hqvideo.dmg"
-tar_dS="-dSYM.tar.gz"
+tar_dS="-dSYM.tgz"
 
 # Die if empty
 if [ -z ${rtag} ]; then
@@ -19,55 +19,51 @@ fi
 # Checkout
 cd "${pathd}"
 if ! git checkout -f ${rtag}; then
-    echo "Unable to checkout ${rtag}"
-    exit 1
+	echo "Unable to checkout ${rtag}"
+	exit 1
 fi
-git_sr=`git rev-parse -q --short --verify HEAD`
 
 # Build
 cd ./macosx
 if [ "${prerun}" == "pre" ]; then
-    if ! xcodebuild -project Warzone.xcodeproj -target Warzone -configuration Release -PBXBuildsContinueAfterErrors=NO; then
-        open .
-    fi
-    exit 0
+	if ! xcodebuild -project Warzone.xcodeproj -target Warzone -configuration Release -PBXBuildsContinueAfterErrors=NO; then
+		open .
+	fi
+	exit 0
 elif ! xcodebuild -project Warzone.xcodeproj -parallelizeTargets -target "Make DMGs for Release" -configuration Release; then
-    if ! xcodebuild -project Warzone.xcodeproj -parallelizeTargets -target "Make DMGs for Release" -configuration "Release" -PBXBuildsContinueAfterErrors=NO; then
-	    echo "The build has failed with the preceding error."
-	    exit 1
+	if ! xcodebuild -project Warzone.xcodeproj -parallelizeTargets -target "Make DMGs for Release" -configuration "Release" -PBXBuildsContinueAfterErrors=NO; then
+		echo "The build has failed with the preceding error."
+		exit 1
 	fi
 fi
 
 # Rename and md5
+rtag=$(echo ${rtag} | sed 's:/:_:g')
+git_sr=$(../build_tools/autorevision -f -s VCS_SHORT_HASH -o ../src/autorevision.cache)
 cd build/dmgout/out
-rtag=`echo ${rtag} | sed 's:/:_:g'`
 
-if [ -f "${dmg_bn}${dmg_nv}" ]; then
-    mv "${dmg_bn}${dmg_nv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}"
-    md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}.md5"
+if [ -f "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}" ]; then
+	md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}">"${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}.md5"
 else
-    echo "${dmg_bn}${dmg_nv} does not exist, skipping"
+	echo "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS} does not exist, skipping"
 fi
 
-if [ -f "${dmg_bn}${dmg_lv}" ]; then
-    mv "${dmg_bn}${dmg_lv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}"
-    md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}.md5"
+if [ -f "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}" ]; then
+	md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv}.md5"
 else
-    echo "${dmg_bn}${dmg_lv} does not exist, skipping"
+	echo "${dmg_bn}-${rtag}_[${git_sr}]${dmg_nv} does not exist, skipping"
 fi
 
-if [ -f "${dmg_bn}${dmg_hv}" ]; then
-    mv "${dmg_bn}${dmg_hv}" "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}"
-    md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}.md5"
+if [ -f "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}" ]; then
+	md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv}.md5"
 else
-    echo "${dmg_bn}${dmg_hv} does not exist, skipping"
+	echo "${dmg_bn}-${rtag}_[${git_sr}]${dmg_lv} does not exist, skipping"
 fi
 
-if [ -f "${dmg_bn}${tar_dS}" ]; then
-    mv "${dmg_bn}${tar_dS}" "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}"
-    md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}">"${dmg_bn}-${rtag}_[${git_sr}]${tar_dS}.md5"
+if [ -f "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}" ]; then
+	md5 -q "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}">"${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv}.md5"
 else
-    echo "${dmg_bn}${tar_dS} does not exist, skipping"
+	echo "${dmg_bn}-${rtag}_[${git_sr}]${dmg_hv} does not exist, skipping"
 fi
 # Open upload app
-open /Applications/FileZilla.app
+open -b de.filezilla
